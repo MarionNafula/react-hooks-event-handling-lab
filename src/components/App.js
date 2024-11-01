4f50fe5a05e6153c6ed9563803d68a2dc3f2ad7f
Marion <wekesamarion100@gmail.com>
2024-11-01 16:01:06 +0300

    completed lab

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import EyesOnMe from "./EyesOnMe";
+import Keypad from "./Keypad";
+
+
+function App() {
+    return (
+        <div>
+            <Keypad />
+            <EyesOnMe />
+        </div>
+    );
+}
+
+export default App
